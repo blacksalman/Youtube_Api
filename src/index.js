2cd352b53e2c6f292146e58036cacc222b3743b0
blacksalman <salman51180189@gmail.com>
2024-01-30 23:22:15 +0530

    setup the finitial folder

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import connectDB from "./db";
+import { app } from './app.js';
+dotenv.config({path: './.env'});
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 6001, ()=> {
+        console.log(`server is running at port : ${process.env.PORT}`);
+    });
+})
+.catch((err)=> {
+    console.log('Mongodb connection failed', err);
+})
